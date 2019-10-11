a8bf24790217767bd25fae8ab638cddef04e19e0
Matt Hill <hil32c@batman-bm.nexus.csiro.au>
2019-10-11 16:47:06 +1100

    added vignette file (empty)

--- R/weather_functions.R
+++ R/weather_functions.R
@@ -110,3 +110,120 @@ cherry_dev <- function(tlow = 4.5, thigh = 100, tempvec = tempvec, budburst = "2
 
     return(degday)
 }
+
+#' Hourly temperature interpolation function
+#'
+#' This function interpolates the daily minimum and maximum temperature values to predict hourly temperature values.
+#' The function uses the location, the \code{suncalc} package and a vector of dates to calculate solar noon and sunrise times (hour).
+#' Adjusts the sine coefficients to the location information, and from there assumes Tmin is achieved at
+#' sunrise-1 hour and Tmax is achieved at solar noon + 2 hours.
+#'
+#' Original calculations are found in chapter 2 of Campbell & Norman (1998)
+
+#' @param climate A dataframe with columns of: (Date, Latitude, Longitude, Tmin, Tmax)
+#' @export
+#' @return An expanded data.frame of \code{climate} with 1 row per hour of each day provided, with corresponding temperature value
+#' @author Madeleine Barton
+#' @author Matt Hill
+#' @references Campbell, G. S., & Norman, J. M. (1998). An Introduction to Environmental Biophysics. https://doi.org/10.1007/978-1-4612-1626-1
+
+
+hourly.interpolate <- function(climate=climate){
+
+    if ("DateCleared" %in% colnames (climate)){
+        colnames(climate)[which(names(climate) == "DateCleared")] <- "Date"
+    }
+
+    if ("X" %in% colnames (climate)){
+        colnames(climate)[which(names(climate) == "X")] <- "Longitude"
+    }
+
+    if ("Y" %in% colnames (climate)){
+        colnames(climate)[which(names(climate) == "Y")] <- "Latitude"
+    }
+
+    lat.long <- c(climate$Latitude[1], climate$Longitude[1])
+
+    #-----------------------------------------------------------------------------------------------------------
+    #Convert Daily data to hourly data using the max/min temperatures and a sin equation
+    #-----------------------------------------------------------------------------------------------------------
+    numdays<-length(climate[,1])
+
+    ## calculate solar noon for a give location using the suncalc package
+    solar_params <- suncalc::getSunlightTimes(date = as.Date(climate$Date, origin="1970-01-01"),
+                                              lat = lat.long[1], lon = lat.long[2], tz ="Australia/Sydney",
+                                              keep=c("solarNoon", "sunrise"))
+
+    climate$solar_noon <- solar_params$solarNoon
+    climate$sunrise <- solar_params$sunrise
+
+    ## calculate the time difference from solar noon
+    time_diff <- climate$solar_noon
+
+    ## set to 12:00, midday
+    lubridate::hour(time_diff) <-12; lubridate::minute(time_diff) <-0; lubridate::second(time_diff) <-0
+
+    ##calculate difference
+    climate$t.adj <- as.numeric(difftime(climate$solar_noon,time_diff,units="hours"))
+
+    climate<-climate[rep(seq_len(nrow(climate)), each=24),1:ncol(climate)]
+    climate$hour<-rep((1:24),numdays)
+
+    #total number of hours in the simulations
+    numhours<-length(climate[,1])
+
+    ## From equation 2.2
+    w <- pi/12
+    sin.coeff <- vector()
+    for (i in 1:numhours){
+        sin.coeff[i] <- 0.44-0.46*sin(w*(climate$hour[i]+climate$t.adj[i])+0.9)+0.11*sin(2*(w*(climate$hour[i]+climate$t.adj[i]))+0.9)
+        print (i)
+    }
+
+    #Create vectors for processing
+
+    Tmin<-climate$Tmin    #saving the minimum temperature as its own object, for easier use below
+    Tmax<-climate$Tmax    #saving the maximum temperature as its own object, for easier use below
+    hour <- climate$hour  #daily hours for iterations
+    sunrise <- as.integer(format(round(climate$sunrise, units="hours"), format="%H")) # hour of sunrise - rounded to nearest hour
+    solar_noon <- as.integer(format(round(climate$solar_noon, units="hours"), format="%H")) # hour of solar_noon - rounded to nearest hour
+
+    ## Loop to iterate over all the hours in the dataset provided, and use the above vectors to calculate a "Ta" per hour
+
+    for(i in 1:numhours){ ###START LOOP (note the + sign in the console (bottom left window))
+        if(i>=25){
+            j<-i-24 ## j refers to the max/min conditions of the previous day
+        }else{
+            j<-i} ##  the max/min are assumed to be the same as on that day
+
+        if(i<=(numhours-24)){
+            k<-i+24## 'j'refers to the  conditions of the following day (for the final day in the script, the max/min are assumed to be the same as on that day)
+        }else{
+            k<-i}##  the max/min are assumed to be the same as on that day
+        if (hour[i]<(sunrise[i]-1)) {                                         #hours before sunrise-1
+            Ta<-(sin.coeff[i]*Tmax[j])+((1-sin.coeff[i])*Tmin[i])}
+        if((hour[i]>=(sunrise[i]-1)|hour[i]<=(solar_noon[i]+2))){                            #hours between sunrise-1 and solar noon + 2
+            Ta<-(sin.coeff[i]*Tmax[i])+((1-sin.coeff[i])*Tmin[i])}
+        if(hour[i]>(solar_noon[i]+2)){                                          #hours after solar noon + 2
+            Ta<-(sin.coeff[i]*Tmax[i])+((1-sin.coeff[i])*Tmin[k])}
+        if(i==1){
+            hourly<-as.data.frame(Ta)
+        }else{
+            hourly<-rbind(hourly,Ta)
+        }
+        print(i)
+    } ###END LOOP
+
+    ##calculating the total number of hours in the simulation.
+    julhour<-1:numhours
+
+    ##creating a data table with the hourly temperatures and the total number of hours (NOTE: to bind columns, the length of the columns must always be the same)
+    hour.climate.data<-as.data.frame(cbind(julhour, climate, hourly))
+
+    hour.climate.data$hour <- seq(from =0, to=23, by=1)
+
+    hour.climate.data <- hour.climate.data[,c("Date",  "Latitude", "Longitude", "Tmin", "Tmax", "Ta")]
+
+    return (hour.climate.data)
+}
+
