5a471e7ba74cc20a56436dacb38b732a538f2985
mattecologist <matt.hill@csiro.au>
2019-12-19 16:26:14 +1100

    added flyobs to data and added progress bars to app

--- R/tephdev_vis.R
+++ R/tephdev_vis.R
@@ -1,11 +1,15 @@
 #' Shiny app to demonstrate model and data integration
 #'
-#' I development!
+#' In development!
+#' 
+#' Download the SILO data from https://www.longpaddock.qld.gov.au/silo/gridded-data/
+#' For this demonstration, use the 2015 min and max temperatures
+#' 
 #' @export
 #' @return A Shiny app instance
 #' @author Matt Hill
 
-TephDev_vis <- function() {
+TephDev_vis <- function(silo_dir=silo_dir) {
   
   require (shiny)
   require(shinyjs)
@@ -18,6 +22,26 @@ TephDev_vis <- function() {
   require (mapview)
   require(TephDev)
   
+  Tmin2015 <- raster::brick(paste0(silo_dir, "2015.min_temp.nc"))
+  Tmax2015 <- raster::brick(paste0(silo_dir, "2015.max_temp.nc"))
+
+  datevec <- as.Date(Tmin2015@z$Date)
+  
+  # Simple function to create average (although not entirely true) daily temps.
+  
+  create_Ta <- function(x, y, ext){
+    x <- raster::crop (x, ext)
+    y <- raster::crop (y, ext)
+    z <- (x+y)/2
+    names(z) <- names(x)
+    return(z)
+  }
+  
+  flyobs <- read_csv("/home/hil32c/Documents/raster_process/app_data/flypoints.csv") %>%
+    st_as_sf(coords = c("Longitude", "Latitude"),
+             crs = "+proj=longlat +datum=WGS84")
+  
+## Begin app.  
   shinyApp(
     ui = navbarPage(
       theme = shinythemes::shinytheme("superhero"),
@@ -35,46 +59,20 @@ TephDev_vis <- function() {
                  br(),
                  sliderInput("slider2", label = h4("Select Commencement Date"), min = min(as.Date(Tmin2015@z$Date)),
                              max = max(as.Date(Tmin2015@z$Date)),
-                             value=mean(max(as.Date(Tmin2015@z$Date))),
+                             value=min(as.Date(Tmin2015@z$Date)),
                              timeFormat="%Y-%m-%d"),
                  p("followed by a submit button to perform task"),
                  br(),
                  actionButton('submit_button', 'Run Analysis' ),
+                 br())),
+                 tabPanel('Displaying Model outputs',
                  br(),
-                 p("For now, just a radio button to select preconstructed rasters"),
-                 radioButtons( 'season', 'Select Season', choices = list( '2015/16' = 'X2015.06.01_X2015.12.31', '2016/17' = 'X2016.06.01_X2016.12.31', '2017/18'='X2017.06.01_X2017.12.31' ),inline = T )
-               )),
-      tabPanel('Displaying Model outputs',
-               br(),
-               actionButton('add_button', 'Add layers'),
-               leafletOutput("mapplot", height=600, width=800) ## size here really determines speed of loading
+                 actionButton('add_button', 'Add layers'),
+                 leafletOutput("mapplot", height=600, width=800) ## size here really determines speed of loading
       )
       
     ), 
     server = function( input, output, session ){
-      
-      Tmin2015 <- raster::brick("./SILO/2015.min_temp.nc")
-      Tmax2015 <- raster::brick("./SILO/2015.max_temp.nc")
-      
-      datevec <- as.Date(Tmin2015@z$Date)
-      
-      create_Ta <- function(x, y, ext){
-        x <- raster::crop (x, ext)
-        y <- raster::crop (y, ext)
-        z <- (x+y)/2
-        names(z) <- names(x)
-        return(z)
-      }
-      
-      #QFFfree <- st_read("./app_data/qff_control/QFly_Control_Zone.shp")
-      
-      maxQFF <- raster("./app_data/qfly_current.tif")
-      projection(maxQFF) <- "+proj=aea +lat_1=-18 +lat_2=-36 +lat_0=0 +lon_0=132 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
-      
-      flyobs <- read_csv("./app_data/flypoints.csv") %>%
-        st_as_sf(coords = c("Longitude", "Latitude"),
-                 crs = "+proj=longlat +datum=WGS84")
-      
       # Select map
       output$map <- renderLeaflet({
         leaflet() %>% 
@@ -91,24 +89,28 @@ TephDev_vis <- function() {
       tempvec <- reactiveVal(NULL)
       tempdata <- reactiveVal(NULL)
       r2 <- reactiveVal(NULL)
-      
-      
-      
+      sim.length <- reactiveVal(NULL)
+
       #27700 
       observeEvent(input$map_draw_new_feature, {
+        
+        withProgress(message = 'Preparing spatial data', value = 0, {
         feat <- input$map_draw_new_feature
         coords <- unlist(feat$geometry$coordinates)
         coords <- matrix(coords, ncol = 2, byrow = T)
+        incProgress(1/3, detail = paste("Bounding layers"))
         poly <- st_sf(st_sfc(st_polygon(list(coords))), crs = st_crs("+proj=longlat +datum=WGS84"))
         # use Extent not BBOX
         bbox <- extent(poly)
         bboxRV(bbox)
+        incProgress(2/3, detail = paste("Creating Ta"))
         TempR <- create_Ta(Tmax2015, Tmin2015, bbox)
         Ta2015(TempR)
-        
+       
+        incProgress(1/3, detail = paste("Preparing outputs"))
         leafletProxy("map", session) %>%
           clearImages()
-        print(Ta2015)
+        })
         
       })
       
@@ -121,28 +123,27 @@ TephDev_vis <- function() {
       ###################################################################
       #create a blank raster stack to store output to.
       observeEvent(input$submit_button, {
-        
-        
-        # r1 <- raster::brick(Ta2015())
-        # print(r1)
-        
-        
+    
         # init a progress bar
         pb = txtProgressBar(min = 1, max = (nrow(Ta2015())*ncol(Ta2015())), width=20, initial = 1)
         
         test <- list()
         tempdata(Ta2015()[])
         
+        sim.length(datevec >= as.Date(input$slider2))
         # Take the raster brick, and consider each cell postion in a single layer.
         # 
-        
+        withProgress(message = 'Running models', value = 0, {
         for (i in 1:(nrow(Ta2015())*ncol(Ta2015()))) {
+          incProgress(1/(nrow(Ta2015())*ncol(Ta2015())), detail = paste("Cell", i, "of", (nrow(Ta2015())*ncol(Ta2015()))))
           
           tempvec(as.vector(tempdata()[i, ]))
           
           if(is.na(tempvec()[1])==FALSE) {
             
-            hold.dat <- yonow_model(tempvec(), datevec, start.date = as.Date("2015-01-01")) # running from 1 Jan only at this point - need to update this
+           # hold.dat <- yonow_model(tempvec(), datevec, start.date = as.Date("2015-01-01")) # running from 1 Jan only at this point - need to update this
+            
+            hold.dat <- yonow_model(tempvec(), datevec, start.date = as.Date(input$slider2))
             
             hold.dat$value <- 1 #give thme all a value of 1 - indicating that it was still developing on this day.
             xx <- length(tempvec())-length(hold.dat$value)
@@ -153,40 +154,28 @@ TephDev_vis <- function() {
           else{
             test[[paste0(i)]] <- rep(NA, length(tempvec()))
           }
-          
+        
           setTxtProgressBar(pb,i)
         }
         
-        
         # step through each vector position and assign to the appropriate raster layer
-        # this changes it from requiring the number of cells * replacement to the number of days * replacement = much quicker!!
-        #print(as.vector(unlist(lapply(test, `[[`, 1))))
-        
+        # this changes it from requiring the number of cells * replacement to the number of days * replacement = much quicker!
         r1 <- Ta2015()
         r1[] <- NA
         
-        for (ii in 1:length(tempvec())){
+        message(paste0(as.Date(input$slider2), " selected as start"))
+       # for (ii in 1:length(tempvec())){
+        for (ii in which(datevec >= as.Date(input$slider2))){
+          incProgress(1/length(which(datevec >= as.Date(input$slider2))), detail = paste("Raster building", ii))
           r1[[ii]] <- as.vector(unlist(lapply(test, `[[`, ii)))
-          print (ii)
+         
         }
         
         r2(mask(sum(r1, na.rm=T), Ta2015()[[1]]))
-        
+        })
         
       })
       ###################################################################
-      
-      
-      
-      
-      
-      ## Build up mapview raster objects
-      
-      Overwinter <-raster(paste0("./app_data/overwinter_X2015.06.01_X2015.12.31.grd"))
-      
-      m <- mapview(Overwinter)
-      
-      
       ## Leaflet rendering
       
       output$mapplot <- renderLeaflet({
@@ -208,7 +197,6 @@ TephDev_vis <- function() {
           addRasterImage(r2(),
                          project=TRUE)%>%
           ## Need to come up with a way to add this is a mapview object so that it scales with the map
-          #mapView(m@map)%>%
           addCircles(data= flyobs,
                      layerId = NULL, group = NULL, stroke = TRUE, color = "yellow",
                      weight = 5, opacity = 0.5, fill = FALSE,
