1711d6ae68a40557cc841a68c2e0334f389039c4
Hill, Matt (Data61, Black Mountain) <Matt.Hill@csiro.au>
2020-11-13 11:46:14 +1100

    na.rm = T addition

--- R/medfly_dd.R
+++ R/medfly_dd.R
@@ -13,15 +13,15 @@ medfly_dd <- function (temps=temps,
   
   begin.date <- as.Date(begin.date, origin="1970-01-01")
   
-      egg <- cbind(day_degree_simple(9.3, 100, temps, start.date=begin.date), "Stage" ="egg")
-      larva <- cbind(day_degree_simple(11.1, 100, temps, start.date=min(egg$Date[egg$CDD > 44])), "Stage" ="larva")
-      pupa <- cbind(day_degree_simple(8.4, 100, temps, start.date=min(larva$Date[larva$CDD > 162])), "Stage" ="pupa")
-      adult <- cbind(day_degree_simple(12.8, 100, temps, start.date=min(pupa$Date[pupa$CDD > 56])), "Stage" ="adult")
-      
-      all_data <- cbind(rbind(egg[egg$Date < min(larva$Date),], 
-                              larva[larva$Date < min(pupa$Date),],
-                              pupa[pupa$Date < min(adult$Date),],
-                              adult[adult$Date < min(adult$Date[adult$CDD > 36 ]),]))
-
+  egg <- cbind(day_degree_simple(9.3, 100, temps, start.date=begin.date), "Stage" ="egg")
+  larva <- cbind(day_degree_simple(11.1, 100, temps, start.date=min(egg$Date[egg$CDD > 44], na.rm=T)), "Stage" ="larva")
+  pupa <- cbind(day_degree_simple(8.4, 100, temps, start.date=min(larva$Date[larva$CDD > 162], na.rm=T)), "Stage" ="pupa")
+  adult <- cbind(day_degree_simple(12.8, 100, temps, start.date=min(pupa$Date[pupa$CDD > 56], na.rm=T)), "Stage" ="adult")
+  
+  all_data <- cbind(rbind(egg[egg$Date < min(larva$Date),], 
+                          larva[larva$Date < min(pupa$Date),],
+                          pupa[pupa$Date < min(adult$Date),],
+                          adult[adult$Date < min(adult$Date[adult$CDD > 36 ]),]))
+  
   return (all_data)
 }
